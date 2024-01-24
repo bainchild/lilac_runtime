eb249b60926393e0ea463a1854548a73606a3367
bainchild <bainchild@letterbomb.ftp.sh>
2024-01-23 22:24:04 -0600

    call changes

--- src/lilac_run.lua
+++ src/lilac_run.lua
@@ -1,11 +1,8 @@
-#!/usr/bin/lua5.3
+#!/usr/bin/env lua5.4
 local file = assert((...),"No file given. (use lua require path format)")
 local C = require("c_runtime.C_ffi");
 local en = require('c_runtime.libc');
 _G.____C = C;
-for i,v in next, en do
-   _G[i]=v
-end
 local args = {...}
 local argc,argv = C.Obj(#args), C.Ptr(C.Ptr(C.Obj((function()
    local n = {}
@@ -15,6 +12,7 @@ local argc,argv = C.Obj(#args), C.Ptr(C.Ptr(C.Obj((function()
    return n
 end)())))
 local ex = require(file)
+for i,v in next, en do ex[i]=v end
 if ex.main then -- the __index=_ENV is saving this
    ex.main(argc,argv)
 else
